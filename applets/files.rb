da2b3504439046274499bc4a207572cefc4a110c
Michael Granger <ged@FaerieMUD.org>
2004-05-22 06:43:52 +0000

    Initial checkin.
    --HG--
    extra : convert_revision : svn%3Ab4f3b83b-18d6-0310-a2de-c8f40de7a630/Arrow/trunk%4055

---
+++ applets/files.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/ruby
+# 
+# This file contains the LoadedFiles class, a derivative of Arrow::Applet. It
+# displays the disposition of all the files that are being monitored by Arrow.
+# 
+# == Subversion Id
+#
+#  $Id$
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+
+require 'arrow/applet'
+
+
+### It displays the disposition
+class LoadedFiles < Arrow::Applet
+
+	# SVN Revision
+	SVNRev = %q$Rev$
+
+	# SVN Id
+	SVNId = %q$Id$
+
+	# SVN URL
+	SVNURL = %q$URL$
+
+	# Applet signature
+	Signature = {
+		:name => "FileMap",
+		:description => "It displays the disposition of all the file that are" +
+			" being monitored by Arrow.",
+		:maintainer => "ged@FaerieMUD.org",
+		:defaultAction => 'filemap',
+		:templates => {
+			:filemap => 'filemap.tmpl',
+		}
+	}
+
+
+
+	######
+	public
+	######
+
+	action( 'filemap' ) {|txn, *args|
+		templ = txn.templates[:filemap]
+		templ.txn = txn
+
+		return templ
+	}
+
+
+end # class LoadedFiles
+
+
