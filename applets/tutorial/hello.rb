3895d2fad321b9e922723f7caa61bcf89e4bc45f
Michael Granger <ged@FaerieMUD.org>
2004-09-01 02:37:10 +0000

    Initial checkin of tutorial applet code.
    --HG--
    extra : convert_revision : svn%3Ab4f3b83b-18d6-0310-a2de-c8f40de7a630/trunk%40190

---
+++ applets/tutorial/hello.rb
@@ -0,0 +1,34 @@
+#
+# A "hello world" Arrow applet
+#
+
+require 'arrow/applet'
+
+# Define the applet class; inherit from the base applet class.
+class Hello < Arrow::Applet
+
+	# Applet signature
+	Signature = {
+		:name => "Hello World",
+		:description => "This is yet another implementation of Hello World",
+		:maintainer => "ged@FaerieMUD.org",
+		:defaultAction => 'greet',
+	}
+
+	# Define the 'greet' (default) action
+	def greet_action( txn, name=nil )
+		name ||= "world"
+
+		return <<-EOF
+        <html>
+		<head><title>Hello</title></head>
+		<body><p>Hello, #{name}.</p></body>
+        </html>
+	    EOF
+	end
+
+end
+
+
+
+
