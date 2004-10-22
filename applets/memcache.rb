bf4be66d7de4db4bf1672950fb8674759515b8ed
Michael Granger <ged@FaerieMUD.org>
2004-10-22 02:11:33 +0000

    Initial checkin of memcache applet w/templates.
    --HG--
    extra : convert_revision : svn%3Ab4f3b83b-18d6-0310-a2de-c8f40de7a630/trunk%40228

---
+++ applets/memcache.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/ruby
+# 
+# This file contains the MemCache class, a derivative of Arrow::Applet. This is a web interface to MemCached.
+# 
+# == Subversion Id
+# 
+# $Id$
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+
+require 'arrow/applet'
+require 'memcache'
+
+### This is a web interface to MemCached.
+class MemCacheApplet < Arrow::Applet
+
+	# SVN Revision
+	SVNRev = %q$Rev$
+
+	# SVN Id
+	SVNId = %q$Id$
+
+	# SVN URL
+	SVNURL = %q$URL$
+
+	# Applet signature
+	Signature = {
+		:name => "memcache",
+		:description => "This is a web interface to MemCached.",
+		:maintainer => "ged@FaerieMUD.org",
+		:defaultAction => 'display',
+		:templates => {
+			:display => 'memcache/display.tmpl',
+		},
+	}
+
+	DefaultServers = [ 'localhost:11211' ]
+
+	def initialize( *args )
+		super
+
+		servers = DefaultServers
+		if @config.respond_to?( :memcache )
+			servers.replace( @config.memcache.servers )
+		end
+
+		@memcache = MemCache::new( *servers )
+	end
+
+
+	######
+	public
+	######
+
+	def display_action( txn, *args )
+		templ = self.loadTemplate( :display )
+		templ.txn = txn
+		templ.memcache = @memcache
+
+		return templ
+	end
+
+
+end # class MemCache
+
+
