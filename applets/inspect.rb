45c752e582075d1029d5412ac302581fe9905464
Michael Granger <ged@FaerieMUD.org>
2004-10-26 17:45:57 +0000

    Initial checkin of request inspector applet.
    --HG--
    extra : convert_revision : svn%3Ab4f3b83b-18d6-0310-a2de-c8f40de7a630/trunk%40239

---
+++ applets/inspect.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ruby
+# 
+# This file contains the InspectorApplet class, a derivative of
+# Arrow::Applet. It dumps data that might be useful to applet developers.
+# 
+# == Subversion Id
+# 
+# $Id$
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+
+require 'arrow/applet'
+
+
+### An applet that displays introspection information that might be useful to
+### applet developers.
+class InspectorApplet < Arrow::Applet
+
+	# SVN Revision
+	SVNRev = %q$Rev$
+
+	# SVN Id
+	SVNId = %q$Id$
+
+	# SVN URL
+	SVNURL = %q$URL$
+
+	# Applet signature
+	Signature = {
+		:name => "Inspector",
+		:description => "It dumps data that might be useful to applet developers.",
+		:maintainer => "ged@FaerieMUD.org",
+		:defaultAction => 'display',
+		:templates => {
+			:display => 'inspect/display.tmpl',
+		},
+	}
+
+
+	def display_action( txn, *args )
+		templ = self.loadTemplate( :display )
+
+		templ.txn = txn
+		templ.applet = self
+
+		return templ
+	end
+
+end # class InspectorApplet
+
+
