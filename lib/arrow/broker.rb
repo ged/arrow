2ed27c2afa36ef97579197e9e04ae108b490fce3
Michael Granger <ged@FaerieMUD.org>
2009-09-02 18:29:05 -0700

    Expose Arrow::Broker#run_applet, #run_missing_applet_handler, and #run_error_handler for situations in which an applet needs to do its own error- and missing-applet handling.

--- lib/arrow/broker.rb
+++ lib/arrow/broker.rb
@@ -51,8 +51,8 @@ class Arrow::Broker < Arrow::Object
 
 	# The Time when the Broker was started
 	attr_reader :start_time
-	
-	
+
+
 
 	### Dispatch the specified transaction +txn+ to the appropriate handler
 	### based on the request's path_info.
@@ -88,6 +88,82 @@ class Arrow::Broker < Arrow::Object
 		end
 
 		# self.log.debug "Returning %p" % [ rval ]
+		return rval
+	end
+
+
+	### Run the specified +applet+ with the given +txn+ (an Arrow::Transaction) 
+	### and the +rest+ of the path_info split on '/'.
+	def run_applet( applet, txn, rest )
+		self.log.debug "Running '%s' with args: %p" %
+			[ applet.signature.name, rest ]
+		return applet.run( txn, *rest )
+	rescue ::Exception => err
+		self.log.error "[%s]: Error running %s (%s): %s:\n\t%s" % [
+		    txn.serial,
+			applet.signature.name,
+			applet.class.filename,
+			err.message,
+			err.backtrace.join("\n\t"),
+		]
+		return self.run_error_handler( applet, txn, err )
+	end
+
+
+	### Handle requests that target an applet that doesn't exist.
+	def run_missing_applet_handler( txn, uri )
+		rval = appletchain = nil
+		handlerUri = @config.applets.missingApplet
+		args = uri.split( %r{/} )
+
+		# Build an applet chain for user-configured handlers
+		if handlerUri != "(builtin)"
+			appletchain = @registry.find_applet_chain( handlerUri )
+			self.log.error "Configured MissingApplet handler (%s) doesn't exist" %
+				handlerUri if appletchain.empty?
+		end
+
+		# If the user-configured handler maps to one or more handlers, run
+		# them. Otherwise, run the build-in handler.
+		unless appletchain.nil? || appletchain.empty?
+			rval = self.run_applet_chain( txn, appletchain )
+		else
+			rval = self.builtin_missing_handler( txn, *args )
+		end
+
+		return rval
+	end
+
+
+	### Handle the given applet error +err+ for the specified +applet+, using 
+	### the given transaction +txn+. This will attempt to run whatever applet 
+	### is configured as the error-handler, or run a builtin handler applet 
+	### if none is configured or the configured one isn't loaded.
+	def run_error_handler( applet, txn, err )
+		rval = nil
+		handlerName = @config.applets.errorApplet.sub( %r{^/}, '' )
+
+		unless handlerName == "(builtin)" or !@registry.key?( handlerName )
+			handler = @registry[handlerName]
+			self.log.notice "Running error handler applet '%s' (%s)" %
+				[ handler.signature.name, handlerName ]
+
+			begin
+				rval = handler.run( txn, "report_error", applet, err )
+			rescue ::Exception => err2
+				self.log.error "Error while attempting to use custom error "\
+				"handler '%s': %s\n\t%s" % [
+					handler.signature.name,
+					err2.message,
+					err2.backtrace.join("\n\t"),
+				]
+
+				rval = self.builtin_error_handler( applet, txn, err )
+			end
+		else
+			rval = self.builtin_error_handler( applet, txn, err )
+		end
+
 		return rval
 	end
 
@@ -157,49 +233,6 @@ class Arrow::Broker < Arrow::Object
 	end
 
 
-	### Run the specified +applet+ with the given +txn+ (an Arrow::Transaction) 
-	### and the +rest+ of the path_info split on '/'.
-	def run_applet( applet, txn, rest )
-		self.log.debug "Running '%s' with args: %p" %
-			[ applet.signature.name, rest ]
-		return applet.run( txn, *rest )
-	rescue ::Exception => err
-		self.log.error "[%s]: Error running %s (%s): %s:\n\t%s" % [
-		    txn.serial,
-			applet.signature.name,
-			applet.class.filename,
-			err.message,
-			err.backtrace.join("\n\t"),
-		]
-		return self.run_error_handler( applet, txn, err )
-	end
-
-
-	### Handle requests that target an applet that doesn't exist.
-	def run_missing_applet_handler( txn, uri )
-		rval = appletchain = nil
-		handlerUri = @config.applets.missingApplet
-		args = uri.split( %r{/} )
-
-		# Build an applet chain for user-configured handlers
-		if handlerUri != "(builtin)"
-			appletchain = @registry.find_applet_chain( handlerUri )
-			self.log.error "Configured MissingApplet handler (%s) doesn't exist" %
-				handlerUri if appletchain.empty?
-		end
-
-		# If the user-configured handler maps to one or more handlers, run
-		# them. Otherwise, run the build-in handler.
-		unless appletchain.nil? || appletchain.empty?
-			rval = self.run_applet_chain( txn, appletchain )
-		else
-			rval = self.builtin_missing_handler( txn, *args )
-		end
-
-		return rval
-	end
-
-
 	### The builtin missing-applet handler routine. Returns +false+, which 
 	### causes the dispatcher to decline the request.
 	def builtin_missing_handler( txn, *args )
@@ -208,43 +241,10 @@ class Arrow::Broker < Arrow::Object
 	end
 
 
-	### Handle the given applet error +err+ for the specified +applet+, using 
-	### the given transaction +txn+. This will attempt to run whatever applet 
-	### is configured as the error-handler, or run a builtin handler applet 
-	### if none is configured or the configured one isn't loaded.
-	def run_error_handler( applet, txn, err )
-		rval = nil
-		handlerName = @config.applets.errorApplet.sub( %r{^/}, '' )
-
-		unless handlerName == "(builtin)" or !@registry.key?( handlerName )
-			handler = @registry[handlerName]
-			self.log.notice "Running error handler applet '%s' (%s)" %
-				[ handler.signature.name, handlerName ]
-
-			begin
-				rval = handler.run( txn, "report_error", applet, err )
-			rescue ::Exception => err2
-				self.log.error "Error while attempting to use custom error "\
-				"handler '%s': %s\n\t%s" % [
-					handler.signature.name,
-					err2.message,
-					err2.backtrace.join("\n\t"),
-				]
-
-				rval = self.default_error_handler( applet, txn, err )
-			end
-		else
-			rval = self.default_error_handler( applet, txn, err )
-		end
-
-		return rval
-	end
-
-
 	### The builtin error handler routine. Outputs a plain-text backtrace
 	### for the given exception +err+ and +applet+ to the given
 	### transaction +txn+.
-	def default_error_handler( applet, txn, err )
+	def builtin_error_handler( applet, txn, err )
 		self.log.notice "Using builtin error handler."
 		txn.request.content_type = "text/plain"
 		txn.status = Apache::OK
